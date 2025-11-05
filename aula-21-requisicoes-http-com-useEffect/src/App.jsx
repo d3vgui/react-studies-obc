07a3128118c69a68eaf599e53d169132818bc03c
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-11-04 21:56:33 -0300

    exemplo de código aula 21: requisições http com useEffect

---
+++ aula-21-requisicoes-http-com-useEffect/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+async function fetchPokemon() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon")
+  const data = await response.json()
+  return data.results
+}
+
+export default function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    fetchPokemon().then(results => {
+      console.log("Requisição realizada.")
+      console.log(results)
+      setPokemon(results)
+    })
+  }, [])
+
+  return (
+    <div className="app">
+      <div>
+        <h2>Pokémon</h2>
+        {JSON.stringify(pokemon)}
+      </div>
+    </div>
+  )
+}
